8665e4c3bcdd3cfb2b25d9ca421e2e7bfc2b0a25
Rafael <rafaelcsadm@hotmail.com>
2023-08-25 18:25:53 -0300

    criação dos arquivos principais

---
+++ Main/src/application/ContaTerminal.java
@@ -0,0 +1,19 @@
+package application;
+
+import intites.BankAccount;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //TODO: conhecer e importar as classes incluindo o scanner
+        BankAccount account = new BankAccount();
+        Scanner sc = new Scanner(System.in);
+        //TODO: exibir as menssagens para o nosso usuario
+
+        //TODO: Obter pelo scanner os valores digitados no terminal
+
+        //TODO: exibir a mensagem conta criada
+
+    }
+}
